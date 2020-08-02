8e480c37c352ff3c5f87ff7382e411b967e9aa0b
Araya <eduardo.jezine@webjump.com.br>
2020-08-02 17:08:21 -0300

    Adding menu mobile and ajustment

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function toggleMenu(close = false) {
+    const menu = document.getElementById('menu-mobile');
+    if (close) {
+        return menu.classList.remove('active');
+    }
+    if (!menu.classList.contains('active')) {
+        menu.classList.add('active');
+    } else {
+        menu.classList.remove('active');
+    }
+}
