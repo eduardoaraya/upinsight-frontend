7456c12beb7134ef3be22f7dded31b906518b788
Araya <eduardo.jezine@webjump.com.br>
2020-08-29 14:15:15 -0300

    Feature for the top menu when the scrolling to find the final page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,22 @@
-import {
-  initMenuMobile
-} from './menu.js';
+import Menu from './menu.js';
+import ScrollEvent from './scroll.event.js';
 
-initMenuMobile();
+// Menu init 
+const menuConfig = new Menu({
+  desktop: 'header-fixed',
+  mobile: 'menu-mobile',
+  targetMobile: '[data-menu-mobile]'
+});
+menuConfig.init();
+
+const sizeDocument = document.body.scrollHeight;
+const target = (sizeDocument - window.innerHeight);
+ScrollEvent((menu) => {
+  if (window.scrollY >= target) {
+    menu.up();
+  } else {
+    menu.down();
+  }
+}, menuConfig.animationDesktop(window.innerWidth));
+
 
--- assets/js/scroll.event.js
+++ assets/js/scroll.event.js
@@ -1,5 +1,16 @@
-export default () => {
-  window.addEventListener('scroll', () => {
-
-  })
+function debounce(callback, args, delay) {
+  let timer = null;
+  return () => {
+    if (timer) {
+      clearInterval(timer);
+    }
+    timer = setTimeout(() => {
+      callback(...args);
+      timer = null;
+    }, delay);
+  }
+}
+export default (callback, ...args) => {
+  const handle = debounce(callback, args, 100);
+  window.addEventListener('scroll', handle)
 }
